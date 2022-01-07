ea2a9ade42ff3dcb355cfd153b502a55dce6ef51
Amandeep <ar62634822@gmail.com>
2022-01-07 16:22:52 +0530

    Valorant randomizer

---
+++ lib/main.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+import 'package:shake/shake.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.black,
+        appBar: AppBar(
+          backgroundColor: Colors.red[900],
+          title: const Center(
+            child: Text(
+              'Valorat Randomizer',
+              style: TextStyle(
+                color: Colors.black,
+              ),
+            ),
+          ),
+        ),
+        body: const ValorantRandomizer(),
+      ),
+    ),
+  );
+}
+
+class ValorantRandomizer extends StatefulWidget {
+  const ValorantRandomizer({Key? key}) : super(key: key);
+
+  @override
+  _ValorantRandomizerState createState() => _ValorantRandomizerState();
+}
+
+class _ValorantRandomizerState extends State<ValorantRandomizer> {
+  get child => null;
+
+  int valorantAgent = 4;
+  void agentRandomizer() {
+    setState(() {
+      valorantAgent = Random().nextInt(12) + 1;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    ShakeDetector.autoStart(
+      onPhoneShake: () {
+        agentRandomizer();
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Expanded(
+        child: FlatButton(
+          onPressed: () {
+            agentRandomizer();
+          },
+          child: Image.asset(
+            'images/undefined$valorantAgent.png',
+          ),
+        ),
+      ),
+    );
+  }
+}
